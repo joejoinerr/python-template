b43c7b3abaecec6d641f6909d35ba106749e9e35
Joe Joiner <2454320+joejoinerr@users.noreply.github.com>
2025-03-09 14:48:33 +0000

    Update get_project_root util and add tests

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,8 @@
+"""Utility functions."""
+
 from pathlib import Path
 
 
 def get_project_root() -> Path:
     """Returns the root directory of the project."""
-    parent = Path(__file__).parent
-    if parent.name == "src":
-        return parent.parent
-    return parent
+    return Path(__file__).parent.parent
